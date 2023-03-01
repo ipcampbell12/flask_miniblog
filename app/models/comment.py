ee5dbbaab8bbdacb3ddc3cc9750821d15122fb4a
ipcampbell12 <ipcampbell12@gmail.com>
2023-03-01 10:11:44 -0800

    relationships

--- app/models/comment.py
+++ app/models/comment.py
@@ -8,3 +8,7 @@ class CommentModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(120), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'posts.id'), unique=True, nullable=False)
+    parent_comment_id = db.Column(db.Integer, db.ForeignKey(
+        'comments.id'), unique=True, nullable=False)
--- app/models/post.py
+++ app/models/post.py
@@ -9,3 +9,7 @@ class PostModel(db.Model):
     title = db.Column(db.String(80), nullable=False, primary_key=True)
     text = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
+    comments = db.Relationship(
+        'CommentModel', back_populates='post', lazy="dynamic", cascade="all,celete")
+    tags = db.Relationship(
+        'TagModel', back_pouplates='post', secondary="posts_tags")
