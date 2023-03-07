ddd68c78e3021f2888e45b7dd0616511cf3b00a2
ipcampbell12 <ipcampbell12@gmail.com>
2023-03-07 11:36:24 -0800

    all tag endpoints working

--- app/resources/tag_resource.py
+++ app/resources/tag_resource.py
@@ -39,7 +39,7 @@ def add_tag_to_post(tag_id, post_id):
 
     return post.to_dict()
 
-
+#this one works
 @tag_bp.route('/posts/<int:post_id>/tags/<int:tag_id>',methods=['DELETE'])
 def remove_tag_from_post(tag_id, post_id):
 
@@ -57,20 +57,21 @@ def remove_tag_from_post(tag_id, post_id):
     
     return {"message":f"The tag with id {tag_id} was removed from the post with id {post_id}"}
 
-
+#this one works
 @tag_bp.route('/tags/<string:tag_name>/posts',methods=['GET'])
 def search_for_posts_by_tagname(tag_name):
     
-    posts = db.session.query(PostModel, posts_tags, TagModel).filter(PostModel.id == posts_tags.c.post_id).filter(posts_tags.c.tag_id == TagModel.id).filter(TagModel.name == tag_name)
+    posts = db.session.query(PostModel, posts_tags).filter(TagModel.name == tag_name).filter(TagModel.id == posts_tags.c.tag_id).filter(posts_tags.c.post_id == PostModel.id).all()
 
     post_list = jsonify([{"post":post[0].to_dict()} for post in posts])
 
     return post_list
 
+#this one works
 @tag_bp.route('/tags/<int:tag_id>/posts',methods=['GET'])
 def search_for_posts_by_tagid(tag_id):
     
-    posts = db.session.query(PostModel, posts_tags, TagModel).filter(PostModel.id == posts_tags.c.post_id and posts_tags.c.tag_id == tag_id).all()
+    posts = db.session.query(PostModel, posts_tags).filter(PostModel.id == posts_tags.c.post_id).filter(posts_tags.c.tag_id == tag_id)
 
     post_list = jsonify([{"post":post[0].to_dict()} for post in posts])
 
